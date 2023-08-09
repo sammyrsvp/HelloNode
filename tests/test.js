ddd7e76c282c7627763424c3b2128aa8b76bb5c0
Sammy <sammy.wyb@gmail.com>
2023-08-09 17:50:00 +1000

    just test

--- tests/test.js
+++ tests/test.js
@@ -4,7 +4,7 @@ var test = require('unit.js');
 var index = require('../index.js');
 
 describe('Tests index', function() {
-  it('verifies successful response', function(done) {
+  it('verifies successful  response', function(done) {
     index.get({ /* event */ }, { /* context */ }, (err, result) => {
       try {
         test.number(result.statusCode).is(200);
